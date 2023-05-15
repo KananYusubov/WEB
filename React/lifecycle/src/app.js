e5df67578fbaf81f283d4dd1e3cff4a5bf4688e8
Kanan Yusubov <kananysbv@gmail.com>
2023-05-15 20:01:53 +0400

    finish lifecycle

--- React/lifecycle/src/app.js
+++ React/lifecycle/src/app.js
@@ -1,27 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 
 function App(props) {
-  const [timer, setTimer] = useState(50);
-  const [salam, setSalam] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimer(timer - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [timer]);
+  //#region LifeCycle Methods
+  // const [timer, setTimer] = useState(50);
+  // const [salam, setSalam] = useState(0);
+  //
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setTimer(timer - 1);
+  //   }, 1000);
+  //
+  //   return () => clearInterval(interval);
+  // }, [timer]);
 
   // useEffect(() => {
   //   console.log("useEffect");
   // }, [timer]);
+  //#endregion  LifeCycle Methods
+
+  const [color, setColor] = useState("black");
+  const changeColor = useRef(null);
+  const change = () => {
+    document.body.style.backgroundColor = changeColor.current.value;
+    setColor(changeColor.current.value);
+  };
 
   return (
     <div>
-      <button onClick={() => setSalam(salam + 1)}>Salam ver</button>
-      <p> Salam: {salam}</p>
-      <p> Timer: {timer}</p>
+      {/*#region LifeCycle */}
+      {/*<button onClick={() => setSalam(salam + 1)}>Salam ver</button>*/}
+      {/*<p> Salam: {salam}</p>*/}
+      {/*<p> Timer: {timer}</p>*/}
       {/*<button onClick={() => setTimer(timer - 1)}>TImer</button>*/}
+      {/*#endregion LifeCycle*/}
+
+      <input type={"text"} ref={changeColor} onChange={change} />
+      <p>{color}</p>
     </div>
   );
 }
