73b797eb5aa0efc6038b143f7e7467c2352bd594
KananYsbvProjects <kenanysbvhomework@gmail.com>
2023-05-19 17:42:05 +0400

    Random planet selection made dynamic

--- React/sw_react/src/Services/api-service.js
+++ React/sw_react/src/Services/api-service.js
@@ -1,42 +1,47 @@
 class ApiService {
-  _apiBaseUrl = "https://swapi.py4e.com/api";
+    _apiBaseUrl = "https://swapi.py4e.com/api";
 
-  async getResources(url) {
-    const response = await fetch(`${this._apiBaseUrl}${url}`);
+    async getResources(url) {
+        const response = await fetch(`${this._apiBaseUrl}${url}`);
 
-    if (!response.ok) {
-      throw new Error(`Could not fetch ${url},received ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`Could not fetch ${url},received ${response.status}`);
+        }
+
+        return await response.json();
     }
 
-    return await response.json();
-  }
+    async getPeople() {
+        const response = await this.getResources(`/people/`);
+        return response.results;
+    }
 
-  async getPeople() {
-    const response = await this.getResources(`/people/`);
-    return response.results;
-  }
+    getPerson(id) {
+        return this.getResources(`/people/${id}`);
+    }
 
-  getPerson(id) {
-    return this.getResources(`/people/${id}`);
-  }
+    async getAllPlanets() {
+        const response = await this.getResources(`/planets/`);
+        return response.results;
+    }
 
-  async getAllPlanets() {
-    const response = await this.getResources(`/planets/`);
-    return response.results;
-  }
+    async getPlanetsCount() {
+        const data = await this.getResources(`/planets/`);
+        return data.count;
+    }
 
-  getPlanet(id) {
-    return this.getResources(`/planets/${id}`);
-  }
+    getPlanet(id) {
+        return this.getResources(`/planets/${id}`);
+    }
 
-  async getAllStarships() {
-    const response = await this.getResources(`/starships/`);
-    return response.results;
-  }
+    async getAllStarships() {
+        const response = await this.getResources(`/starships/`);
+        return response.results;
+    }
 
-  getStarships(id) {
-    return this.getResources(`/starships/${id}`);
-  }
+    getStarships(id) {
+        return this.getResources(`/starships/${id}`);
+    }
 }
 
 export default ApiService;
